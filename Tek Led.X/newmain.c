eb4e6394719b5beec03a1070195575b2cc60a2bd
batuhan osma <bthnosma@gmail.com>
2017-02-20 22:13:18 +0300

    ilk ornek

---
+++ Tek Led.X/newmain.c
@@ -0,0 +1,25 @@
+/*
+ * File:   newmain.c
+ * Author: batuhan osma
+ *
+ * Created on 20 ?ubat 2017 Pazartesi, 10:06
+ */
+
+
+#include <xc.h>
+
+#define _XTAL_FREQ 4000000
+
+void main(void)
+{
+    TRISB0=0;
+    
+    for(;;)
+    {
+        RB0=1;
+        __delay_ms(500);
+        RB0=0;
+        __delay_ms(500);
+    }
+    return;
+}
